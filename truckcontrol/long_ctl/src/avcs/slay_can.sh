d36e6b2dbdbc635940c9894f0ebcb6c7b70bc01c
jspring <jspring@path.berkeley.edu>
2016-04-29 15:25:44 -0700

    modified:   truckcontrol/long_ctl/src/avcs/slay_can.sh Added jbussendGPS

--- truckcontrol/long_ctl/src/avcs/slay_can.sh
+++ truckcontrol/long_ctl/src/avcs/slay_can.sh
@@ -6,6 +6,7 @@ slay  -f -Q -s TERM veh_rcv
 slay  -f -Q -s TERM trk_wr
 slay  -f -Q -s TERM rdj1939
 slay  -f -Q -s TERM gpsdb
+slay  -f -Q -s TERM jbussendGPS
 slay  -f -Q -s TERM jbussend
 slay  -f -Q -s TERM can_man
 slay  -f -Q -s TERM trk_cr
