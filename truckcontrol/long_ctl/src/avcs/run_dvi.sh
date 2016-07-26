2a38080c02573d53586526134316cfe5bdb4e523
jspring <jspring@path.berkeley.edu>
2016-07-25 20:35:42 -0700

    modified:   truckcontrol/long_ctl/src/avcs/run_dvi.sh Added another screen and kills of trk_cr & db_slv

--- truckcontrol/long_ctl/src/avcs/run_dvi.sh
+++ truckcontrol/long_ctl/src/avcs/run_dvi.sh
@@ -45,12 +45,12 @@ echo $DATESTR >/big/data/last_start_timestamp.txt
 /home/truckcontrol/test/db_slv -Q -S `hostname` &
 sleep 1
 /home/truckcontrol/test/trk_cr -t 1000 2>long_input$DATESTR.dbg &
-sleep 1
-/home/can/drv_sja1000/qnx/can_man -n /dev/can1 -s 250 -i 10 -p 0xda000 -e 1 &
-sleep 1
-/home/can/drv_sja1000/qnx/can_man -n /dev/can2 -s 250 -i 7 -p 0xda200 -e 1 &
-sleep 1
-echo "Starting veh_snd from $IPADDR to $ITRIADDR"
+#sleep 1
+#/home/can/drv_sja1000/qnx/can_man -n /dev/can1 -s 250 -i 10 -p 0xda000 -e 1 &
+#sleep 1
+#/home/can/drv_sja1000/qnx/can_man -n /dev/can2 -s 250 -i 7 -p 0xda200 -e 1 &
+#sleep 1
+#echo "Starting veh_snd from $IPADDR to $ITRIADDR"
 #/home/truckcontrol/long_ctl/src/vehcomm/qnx/veh_snd -v -d -i 100 -A $IPADDR -a $ITRIADDR -u 15041 -t $TRUCK >veh_snd.dbg &
 #sleep 1
 #/home/truckcontrol/long_ctl/src/vehcomm/qnx/veh_rcv -v -A $IPADDR -a $ITRIADDR -u 15042 -t $TRUCK >veh_rcv.dbg &
@@ -67,10 +67,15 @@ then
 fi
 OBJDIR=qnx
 echo "Turning on startup screen"
-../vehcomm/$OBJDIR/dvi_snd -c 20 -r 10007 -R 10005 -a $GALAXYWIRELESS -A $IPADDR -E "3 0 0 1 0 0 0 0 0 0" -P "2 1 0 0 0 1 0 0 1 1 1 0 0 0 "
+../vehcomm/$OBJDIR/dvi_snd -c -C 20 -r 10007 -R 10005 -a $GALAXYWIRELESS -A $IPADDR -E "3 0 0 1 0 0 0 0 0 0" -P "0 1 0 0 0 1 0 0 1 1 1 0 0 0 "
+sleep 1
+echo "Turning on startup screen"
+../vehcomm/$OBJDIR/dvi_snd -c -C 20 -r 10007 -R 10005 -a $GALAXYWIRELESS -A $IPADDR -E "3 0 0 1 0 0 0 0 0 0" -P "2 1 0 0 0 1 0 0 1 1 1 0 0 0 "
 sleep 1
 echo "Turning on platoon found screen"
-../vehcomm/$OBJDIR/dvi_snd -c 20 -r 10007 -R 10005 -a $GALAXYWIRELESS -A $IPADDR -E "3 0 0 1 0 0 0 0 0 0" -P "2 1 0 1 0 1 0 0 1 1 1 0 0 0 "
+../vehcomm/$OBJDIR/dvi_snd -c -C 20 -r 10007 -R 10005 -a $GALAXYWIRELESS -A $IPADDR -E "3 0 0 1 0 0 0 0 0 0" -P "2 1 0 1 0 1 0 0 1 1 1 0 0 0 "
 sleep 2
 echo "Turning on startup screen"
-../vehcomm/$OBJDIR/dvi_snd -c 20 -r 10007 -R 10005 -a $GALAXYWIRELESS -A $IPADDR -E "3 0 0 1 0 0 0 0 0 0" -P "2 1 0 0 0 1 0 0 1 1 1 0 0 0 "
+../vehcomm/$OBJDIR/dvi_snd -c -C 20 -r 10007 -R 10005 -a $GALAXYWIRELESS -A $IPADDR -E "3 0 0 1 0 0 0 0 0 0" -P "0 1 0 0 0 1 0 0 1 1 1 0 0 0 "
+slay trk_cr
+slay db_slv
