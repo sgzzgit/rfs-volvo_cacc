b5bf113b7e3cd8c8529636619363686641b683a6
jspring <jspring@path.berkeley.edu>
2016-08-01 21:08:24 -0700

    modified:   trk_cr.c NOW I've added dvi_out_t

--- truckcontrol/long_ctl/src/avcs/trk_cr.c
+++ truckcontrol/long_ctl/src/avcs/trk_cr.c
@@ -56,7 +56,7 @@ static db_id_t db_vars_list[] = {
         {DB_ENGINE_DEBUG_VAR, sizeof(can_debug_t)},
         {DB_ENGINE_RETARDER_DEBUG_VAR, sizeof(can_debug_t)},
         {DB_DVI_RCV_VAR, sizeof(char)},
-        {DB_DVI_OUT_VAR, sizeof(char)},
+        {DB_DVI_OUT_VAR, sizeof(dvi_out_t)},
 };
 
 #define NUM_DB_VARS	sizeof(db_vars_list)/sizeof(db_id_t)
